642837f42e7afe93a5689f1238eb0e50a189a6a2
merino-jorge <jormeran@gmail.com>
2021-12-23 12:05:15 +0100

    test: add missing test

--- src/challenge-22/test.js
+++ src/challenge-22/test.js
@@ -17,6 +17,38 @@ const cases = [
     },
     expected: 6,
   },
+  {
+    input: {
+      value: 1,
+      left: {
+        value: 5,
+        left: {
+          value: 7,
+          left: {
+            value: 3,
+            left: null,
+            right: null,
+          },
+          right: null,
+        },
+        right: null,
+      },
+      right: {
+        value: 6,
+        left: {
+          value: 5,
+          left: null,
+          right: null,
+        },
+        right: {
+          value: 1,
+          left: null,
+          right: null,
+        },
+      },
+    },
+    expected: 28,
+  },
 ]
 
 describe('#22', () => {
