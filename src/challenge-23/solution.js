bb7fa3a0559d3f27dca81d9f99b4e1c3348e78a7
merino-jorge <jormeran@gmail.com>
2021-12-24 10:52:24 +0100

    refactor: update challenge 23

--- src/challenge-23/solution.js
+++ src/challenge-23/solution.js
@@ -1,3 +1,22 @@
 export default function canReconfigure(from, to) {
-  return from.length === to.length && new Set(from).size == new Set(to).size
+  if (from.length !== to.length) {
+    return false
+  }
+
+  const original = {}
+  const converted = {}
+
+  for (let i = 0; i < from.length; i++) {
+    const fromInUse = original[from[i]] && original[from[i]] !== to[i]
+    const toInUse = converted[to[i]] && converted[to[i]] !== from[i]
+
+    if (fromInUse || toInUse) {
+      return false
+    }
+
+    original[from[i]] = to[i]
+    converted[to[i]] = from[i]
+  }
+
+  return true
 }
