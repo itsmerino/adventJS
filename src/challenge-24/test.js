318416939dea89395b5c7fbce6e374afae85c9a4
merino-jorge <jormeran@gmail.com>
2021-12-25 14:13:52 +0100

    test: update challenge 24

--- src/challenge-24/test.js
+++ src/challenge-24/test.js
@@ -1,72 +1,44 @@
 import checkIsSameTree from './solution'
 
+const tree = {
+  value: 1,
+  left: {value: 2, left: null, right: null},
+  right: {value: 3, left: null, right: null},
+}
+
+const tree2 = {
+  value: 1,
+  left: {
+    value: 3,
+    left: {value: 2, left: null, right: null},
+    right: null,
+  },
+  right: {
+    value: 5,
+    left: null,
+    right: {value: 4, left: null, right: null},
+  },
+}
+
 const cases = [
   {
     input: {
-      treeA: {
-        value: 1,
-        left: {value: 2, left: null, right: null},
-        right: {value: 3, left: null, right: null},
-      },
-      treeB: {
-        value: 1,
-        left: {value: 2, left: null, right: null},
-        right: {value: 3, left: null, right: null},
-      },
+      treeA: tree,
+      treeB: tree,
     },
     expected: true,
   },
   {
     input: {
-      treeA: {
-        value: 1,
-        left: {value: 2, left: null, right: null},
-        right: {value: 3, left: null, right: null},
-      },
-      treeB: {
-        value: 1,
-        left: {
-          value: 3,
-          left: {value: 2, left: null, right: null},
-          right: null,
-        },
-        right: {
-          value: 5,
-          left: null,
-          right: {value: 4, left: null, right: null},
-        },
-      },
+      treeA: tree,
+      treeB: tree2,
     },
     expected: false,
   },
   {
     input: {
-      treeA: {
-        value: 1,
-        left: {
-          value: 3,
-          left: {value: 2, left: null, right: null},
-          right: null,
-        },
-        right: {
-          value: 5,
-          left: null,
-          right: {value: 4, left: null, right: null},
-        },
-      },
-      treeB: {
-        value: 1,
-        left: {
-          value: 3,
-          left: {value: 2, left: null, right: null},
-          right: null,
-        },
-        right: {
-          value: 5,
-          left: null,
-          right: {value: 4, left: null, right: null},
-        },
-      },
+      treeA: tree2,
+      treeB: tree2,
     },
     expected: true,
   },
